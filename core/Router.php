30054b91edc79a3b83b33f5fe9455f9874924ca3
Arthur Sadovskiy <arthur.sadovskiy@techs.com.ua>
2017-04-23 18:57:23 +0300

    removed unneded code

--- core/Router.php
+++ core/Router.php
@@ -6,17 +6,6 @@ class Router
 {
     public function route()
     {
-        $config = Application::getConfig();
-        $request = new Request();
-
-        if (empty($request->getController())) {
-            $request->setController($config['routes']['defaultController']);
-
-            $oldAction = $request->getAction();
-            $helpAction = $config['routes']['defaultAction'] . ucfirst($oldAction);
-            $request->setAction($helpAction);
-        }
-
-        return $request;
+        return new Request();
     }
 }
